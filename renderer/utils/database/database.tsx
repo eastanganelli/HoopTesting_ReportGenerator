a5c62010766550cec3c325d8b31c95f40cec63f9
Ezequiel A. Stanganelli <ezequielstanganelli@gmail.com>
2024-06-14 03:01:46 -0300

    small changes in lines

--- renderer/utils/database/database.tsx
+++ renderer/utils/database/database.tsx
@@ -4,30 +4,30 @@ const DatabaseService = {
     CONNECT: (wasClicked: boolean): Promise<any> => {
         return new Promise((resolve, reject) => {
             global.ipcRenderer.send('database-connect', { clicked: wasClicked });
-            global.ipcRenderer.on('database-connected', (event, response: string) => { resolve(response); });
-            global.ipcRenderer.on('database-error', (event, error) => { reject(error); });
+            global.ipcRenderer.on('database-connected', (_, response: string) => { resolve(response); });
+            global.ipcRenderer.on('database-error', (_, error) => { reject(error); });
         });
     },
     ISCONNECTED: (): Promise<any> => {
         return new Promise((resolve, reject) => {
             global.ipcRenderer.send('database-isconnected');
-            global.ipcRenderer.on('database-connected', (event, response: string) => { resolve(response); });
-            global.ipcRenderer.on('database-error', (event, error) => { reject(error); });
+            global.ipcRenderer.on('database-connected', (_, response: string) => { resolve(response); });
+            global.ipcRenderer.on('database-error', (_, error) => { reject(error); });
         });
     },
     SAVE: (address: string, port: number, user: string, password: string): Promise<any> => {
         const dbConfig: DatabaseConfig = { HOST: address, PORT: port, USER: user, PASSWORD: password, DATABASE: 'data_db' };
         return new Promise((resolve, reject) => {
             global.ipcRenderer.send('database-save', dbConfig);
-            global.ipcRenderer.on('database-save-succes', (event, response: string) => { resolve(response); });
-            global.ipcRenderer.on('database-save-error', (event, error) => { reject(error); });
+            global.ipcRenderer.on('database-save-succes', (_, response: string) => { resolve(response); });
+            global.ipcRenderer.on('database-save-error', (_, error) => { reject(error); });
         });
     },
     READ: (): Promise<DatabaseConfig> => {
         return new Promise((resolve, reject) => {
             global.ipcRenderer.send('database-read');
-            global.ipcRenderer.on('database-read-response', (event, response: DatabaseConfig) => { resolve(response); });
-            global.ipcRenderer.on('database-read-error', (event, error) => { reject(error); });
+            global.ipcRenderer.on('database-read-response', (_, response: DatabaseConfig) => { resolve(response); });
+            global.ipcRenderer.on('database-read-error', (_, error) => { reject(error); });
         });
     }
 }
